1dc61f3653182de1c5b1f88f03b80e35e504f063
KnightTheLion <joshuaknightmitchell@gmail.com>
2023-12-24 23:00:10 -0500

    added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/shared/Header';
 import Footer from '@/components/shared/Footer';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -28,6 +29,7 @@ export default function RootLayout({
       <main className='flex-1'>{children}</main>
       <Footer />
       </div>
+      <SpeedInsights />
       </body>
       
     </html>
